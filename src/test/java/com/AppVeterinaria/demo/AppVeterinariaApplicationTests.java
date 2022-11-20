8390308f0a1b5beaee9cacaef4b81034ea09cf17
miguellealperez <mlealp@ucentral.edu.co>
2022-11-20 02:51:52 -0500

    Commit numero 1

---
+++ src/test/java/com/AppVeterinaria/demo/AppVeterinariaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.AppVeterinaria.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVeterinariaApplicationTests {
+
+
+
+}
